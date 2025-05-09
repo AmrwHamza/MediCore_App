aa3ffe91da567de52e90e426bd5fe93e06930f10
AmrwHamza <amrwaleedhamza@gmail.com>
2025-05-09 21:19:29 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MediCoreApp());
+}
+class MediCoreApp extends StatelessWidget {
+  const MediCoreApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
