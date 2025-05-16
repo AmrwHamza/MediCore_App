4fa44c78babcd11893863c2aa5ed91958c68da29
tarek-alasfour-111 <tarekalasfour123@gmail.com.com>
2025-05-16 09:04:33 -0700

    added photos and icon

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -1,9 +1,17 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
-  
+
   /// Assets for imagesStethoscope
   /// assets/images/stethoscope.svg
-  static const String imagesStethoscope = "assets/images/stethoscope.svg";
-}
+  static const String imagesStethoscope =
+      "assets/images/stethoscope_light_mode.svg";
+  static const String boy = "assets/images/boy.svg";
+  static const String girl = "assets/images/girl.svg";
+  static const String otpImage = "assets/images/doctor.svg";
+  static const String doctors = "assets/images/Doctors.svg";
+  static const String jointsDepartment = "assets/images/joints.svg";
+  static const String logoWithoutBackground = "assets/images/Logo_without_background.svg";
+  static const String backgroundAuth = "assets/images/Background_Auth.jpg";
 
+}
