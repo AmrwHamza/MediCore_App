7f735578289f10fb87f0a501852d5d57ffd26607
tarek-alasfour-111 <tarekalasfour123@gmail.com.com>
2025-05-16 09:42:37 -0700

    greate splash view

--- lib/core/helper_functions/on_generate_route.dart
+++ lib/core/helper_functions/on_generate_route.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:medicore_app/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:medicore_app/features/splash/presentation/views/splash_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case SplashView.routeName:
       return MaterialPageRoute(builder: (context) => SplashView());
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (context) => OnBoardingView());
 
     default:
       return MaterialPageRoute(builder: (context) => Scaffold());
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,15 +1,73 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:medicore_app/core/utils/app_images.dart';
+import 'package:medicore_app/features/on_boarding/presentation/views/on_boarding_view.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    _scaleAnimation = Tween<double>(begin: 0.6, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOutBack),
+    );
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
+    );
+
+    _controller.forward();
+
+    Timer(const Duration(milliseconds: 2500), () {
+      Navigator.pushReplacementNamed(
+        context,OnBoardingView.routeName
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff81D2E4),
-      body: Center(child: SvgPicture.asset(Assets.imagesStethoscope)),
+      backgroundColor: const Color(0xff81D2E4),
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Opacity(
+              opacity: _fadeAnimation.value,
+              child: Transform.scale(
+                scale: _scaleAnimation.value,
+                child: SvgPicture.asset(
+                  Assets.imagesStethoscope,
+                  width: 180,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
